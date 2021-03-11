720243610cc2adb7ef1bf7d8747c89a8229ef7b2
HwangSunTae <pozafly@gmail.com>
2021-03-11 15:51:32 +0900

    useInput

---
+++ src/components/InputExam.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useInput } from '../useInput';
+
+const InputExam = () => {
+  const maxLen = (value) => !value.includes('@');
+  const name = useInput('Mr.', maxLen);
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <input placeholder='Name' {...name} />
+    </div>
+  );
+};
+
+export default InputExam;
